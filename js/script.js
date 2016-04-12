b0b08f76a02e8db4852837cd741c6a51de1066e2
bneetv <vibul.neethirajah@bbcag.ch>
2016-04-12 16:05:07 +0200

    Background fertig

--- js/script.js
+++ js/script.js
@@ -10,33 +10,39 @@ $(document).ready(function () {
 
   $('#lightside_title').on({
     mouseover: function(){
-      $("#darkside").insertBefore("#lightside");
+      $("#darksidebackground").insertBefore("#lightsidebackground");
       $("#lightside").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100vh)");
+      $("#lightsidebackground").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100vh)");
     },
     mouseleave: function(){
-      $("#lightside").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100vh)")
+      $("#lightside").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100vh)");
+      $("#lightsidebackground").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100vh)");
     },
     click: function(){
       $("#lightside_title").css("margin-top","30vh");
       $("#lightside_title").css("margin-left","40vw");
       $("#lightside").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100vh)");
+      $("#lightsidebackground").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100vh)");
       $("#lightside_title").off('mouseleave');
     }
   });
 
   $('#darkside_title').on({
     mouseover: function(){
-      $("#lightside").insertBefore("#darkside");
+      $("#lightsidebackground").insertBefore("#darksidebackground");
       $("#darkside").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100vh)");
+      $("#darksidebackground").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100vh)");
     },
     mouseleave: function(){
-      $("#darkside").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100vh)")
+      $("#darkside").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100vh)");
+      $("#darksidebackground").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100vh)");
     },
     click: function(){
       $("#lightside").insertBefore("#darkside");
       $("#darkside_title").css("margin-top","30vh");
       $("#darkside_title").css("margin-left","40vw");
       $("#darkside").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100vh)");
+      $("#darksidebackground").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100vh)");
       $("#darkside_title").off('mouseleave');
     }
   });
