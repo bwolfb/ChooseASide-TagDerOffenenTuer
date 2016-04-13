6868766831f4596acf3a21ebfc81c9a8eb399322
bwolfb <benjamin.wolf@bbcag.ch>
2016-04-13 17:54:50 +0200

    Changed some Style

--- js/script.js
+++ js/script.js
@@ -14,32 +14,34 @@ $(document).ready(function () {
     mouseover: function(){
       if (!onLightside) {
         $("#darksidebackground").insertBefore("#lightsidebackground");
-        $("#lightside").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100vh)");
-        $("#lightsidebackground").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100vh)");
+        $("#lightside").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100%)");
+        $("#lightsidebackground").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100%)");
       }
     },
     mouseleave: function(){
       if (!onLightside) {
-        $("#lightside").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100vh)");
-        $("#lightsidebackground").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100vh)");
+        $("#lightside").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100%)");
+        $("#lightsidebackground").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100%)");
       }
     },
     click: function(){
       onLightside = true;
       $("#lightside_title").css("margin-top","30vh");
       $("#lightside_title").css("margin-left","40vw");
-      $("#lightside").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100vh)");
-      $("#lightsidebackground").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100vh)");
+      $("#lightside").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100%)");
+      $("#lightsidebackground").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100%)");
       $("#lightside").prepend($("<button id=\"lightside_button\"><span>Back </span></button>"));
-      $("#lightside_button").slideDown(600);
+      $("#lightside_button").slideDown(400);
 
       $("#lightside_button").click(function() {
-        $("#lightside").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100vh)");
-        $("#lightsidebackground").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100vh)");
+        $("#lightside").delay(400).css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100%)");
+        $("#lightsidebackground").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100%)");
         $("#lightside_title").css("margin-top","65vh");
         $("#lightside_title").css("margin-left","71vw");
-        $("#lightside_button").slideUp(600);
-        $("#lightside_button").remove();
+        $("#lightside_button").slideUp(400);
+        setTimeout(function() {
+          $("#lightside_button").remove();
+        }, 400);
         onLightside = false;
       });
 
@@ -52,32 +54,34 @@ $(document).ready(function () {
     mouseover: function(){
       if (!onDarkside) {
         $("#lightsidebackground").insertBefore("#darksidebackground");
-        $("#darkside").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100vh)");
-        $("#darksidebackground").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100vh)");
+        $("#darkside").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100%)");
+        $("#darksidebackground").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100%)");
       }
     },
     mouseleave: function(){
       if (!onDarkside) {
-        $("#darkside").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100vh)");
-        $("#darksidebackground").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100vh)");
+        $("#darkside").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100%)");
+        $("#darksidebackground").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100%)");
       }
     },
     click: function(){
       onDarkside = true;
       $("#darkside_title").css("margin-top","30vh");
       $("#darkside_title").css("margin-left","40vw");
-      $("#darkside").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100vh)");
-      $("#darksidebackground").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100vh)");
+      $("#darkside").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100%)");
+      $("#darksidebackground").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100%)");
       $("#darkside").prepend($("<button id=\"darkside_button\"><span>Back </span></button>"));
-      $("#darkside_button").slideDown(600);
+      $("#darkside_button").slideDown(400);
 
       $("#darkside_button").click(function() {
-        $("#darkside").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100vh)");
-        $("#darksidebackground").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100vh)");
         $("#darkside_title").css("margin-top","20vh");
         $("#darkside_title").css("margin-left","5vw");
-        $("#darkside_button").slideUp(600);
-        $("#darkside_button").remove();
+        $("#darkside_button").slideUp(400);
+        setTimeout(function() {
+          $("#darkside_button").remove();
+        }, 400);
+        $("#darkside").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100%)");
+        $("#darksidebackground").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100%)");
         onDarkside = false;
       });
 
