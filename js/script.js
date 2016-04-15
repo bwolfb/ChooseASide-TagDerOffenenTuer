2af0379a16c39481b452e282c710ca3b9fef7fcc
bneetv <vibul.neethirajah@bbcag.ch>
2016-04-15 08:39:06 +0200

    updates

--- js/script.js
+++ js/script.js
@@ -26,18 +26,22 @@ $(document).ready(function () {
     },
     click: function(){
       onLightside = true;
-      $("#lightside_title").css("margin-top","30vh");
+      $("#lightside_title").css("margin-top","2vh");
       $("#lightside_title").css("margin-left","40vw");
       $("#lightside").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100%)");
       $("#lightsidebackground").css("-webkit-clip-path","polygon(0 0, 100% 0, 100% 100%, 0 100%)");
       $("#lightside").prepend($("<button id=\"lightside_button\"><span>Back </span></button>"));
       $("#lightside_button").slideDown(400);
+      $("#lightsidepic").slideDown(800);
 
       $("#lightside_button").click(function() {
-        $("#lightside").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100%)");
-        $("#lightsidebackground").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100%)");
-        $("#lightside_title").css("margin-top","65vh");
-        $("#lightside_title").css("margin-left","71vw");
+        $("#lightsidepic").slideUp(800);
+        setTimeout(function() {
+          $("#lightside").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100%)");
+          $("#lightsidebackground").css("-webkit-clip-path","polygon(65% 0, 100% 0, 100% 100%, 35% 100%)");
+          $("#lightside_title").css("margin-top","20vh");
+          $("#lightside_title").css("margin-left","71vw");
+        },800);
         $("#lightside_button").slideUp(400);
         setTimeout(function() {
           $("#lightside_button").remove();
@@ -47,7 +51,6 @@ $(document).ready(function () {
           $("#lightside").css("background-image","url(\"img/FighterObiHead.png\")");
         }, 700);
       });
-
       $("#lightside").css("background-image","none");
     }
   });
@@ -86,28 +89,33 @@ $(document).ready(function () {
     },
     click: function(){
       onDarkside = true;
-      $("#darkside_title").css("margin-top","30vh");
+      $("#darkside_title").css("margin-top","2vh");
       $("#darkside_title").css("margin-left","40vw");
       $("#darkside").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100%)");
       $("#darksidebackground").css("-webkit-clip-path","polygon(100% 0, 0 0, 0 100%, 100% 100%)");
       $("#darkside").prepend($("<button id=\"darkside_button\"><span>Back </span></button>"));
       $("#darkside_button").slideDown(400);
+      $("#darksidepic").css("diplay","block")
+      $("#darksidepic").slideDown(800);
+
 
       $("#darkside_button").click(function() {
+        $("#darksidepic").slideUp(800);
+        setTimeout(function() {
         $("#darkside_title").css("margin-top","20vh");
         $("#darkside_title").css("margin-left","5vw");
+        $("#darkside").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100%)");
+        $("#darksidebackground").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100%)");
+      },800);
         $("#darkside_button").slideUp(400);
         setTimeout(function() {
           $("#darkside_button").remove();
         }, 400);
-        $("#darkside").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100%)");
-        $("#darksidebackground").css("-webkit-clip-path","polygon(65% 0, 0 0, 0 100%, 35% 100%)");
         onDarkside = false;
         setTimeout(function() {
           $("#darkside").css("background-image","url(\"img/imperator.png\")");
         }, 700);
       });
-
       $("#darkside").css("background-image","none");
     }
   });
